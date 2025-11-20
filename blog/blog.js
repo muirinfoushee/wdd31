a4f5dea20427fc4829132ae6acc3875ebfe83b5f
muirinfoushee <fou23001@byui.edu>
2025-11-20 15:10:27 -0700

    blog pt 2

--- blog/blog.js
+++ blog/blog.js
@@ -10,7 +10,7 @@ const articles = [
     imgAlt: 'Book cover for Septimus Heap 1',
     ages: '10-14',
     genre: 'Fantasy',
-    stars: '****'
+    stars: '⭐⭐⭐⭐'
   },
   {
     id: 2,
@@ -25,6 +25,19 @@ const articles = [
     ages: '12-16',
     genre: 'Fantasy',
     stars: '⭐⭐⭐⭐'
+  },
+  {
+    id: 3,
+    title: 'Belgariad Book One: Pawn of Prophecy',
+    date: 'Feb 12, 2022',
+    author: 'David Eddings',
+    description:
+      "A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf -- set out on a complex mission.",
+    imgSrc: 'https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg',
+    imgAlt: 'Book cover for Pawn of Prophecy',
+    ages: '12-16',
+    genre: 'Fantasy',
+    stars: '⭐⭐⭐⭐⭐'
   }
 ];
 
@@ -35,15 +48,13 @@ function renderArticles() {
 
   articles.forEach(article => {
     const articleEl = document.createElement("article");
+    articleEl.classList.add("article");
 
     const details = document.createElement("div");
     details.classList.add("details");
     details.innerHTML = `
       <p class="date">${article.date}</p>
       <p class="author">By ${article.author}</p>
-      <p class="ages">Ages: ${article.ages}</p>
-      <p class="genre">Genre: ${article.genre}</p>
-      <p class="stars">Rating: ${article.stars}</p>
     `;
 
     const content = document.createElement("div");
@@ -52,6 +63,9 @@ function renderArticles() {
       <h2>${article.title}</h2>
       <img src="${article.imgSrc}" alt="${article.imgAlt}">
       <p>${article.description}</p>
+      <p class="ages">Ages: ${article.ages}</p>
+      <p class="genre">Genre: ${article.genre}</p>
+      <p class="stars">Rating: ${article.stars}</p>
     `;
 
     articleEl.appendChild(details);
